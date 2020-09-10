a9ed0e5f39d37e58b9c553f8fde9dfa50c66096a
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-10 14:42:15 +0200

    Created a User a Form Fields

---
+++ frontend/src/components/AddUser.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+
+class AddUser extends Component{
+    render() {
+        return(
+            <form>
+                <div style={{display:'flex' }}>
+                    <input
+                        type = "text"
+                        name = "name"
+                        placeholder="Name"
+                        style={leftInput}
+                    />
+
+                    <input
+                        type = "text"
+                        name = "surname"
+                        placeholder="Surname"
+                        style={rightInput}
+                    />
+                </div>
+
+                <br/>
+
+                <div style={{display:'flex', }}>
+                    <input
+                        type = "text"
+                        name = "username"
+                        placeholder="Username"
+                        style={leftInput}
+                    />
+
+                    <input
+                        type = "text"
+                        name = "password"
+                        placeholder="Password"
+                        style={rightInput}
+                    />
+                </div>
+                <br/>
+                <div style={{display:'flex' }}>
+                    <input
+                        type = "text"
+                        name = "email"
+                        placeholder="Email"
+                        style={leftInput}
+                    />
+                   <span style={rightInput}></span>
+                </div>
+                <br/>
+                <input
+                    type="submit"
+                    value="Submit"
+                    className="btn"
+                />
+                <br/>
+            </form>
+        )
+    }
+}
+
+const leftInput = {
+    flex:'5',
+    padding:'5px',
+    margin:'10px 10px 0px 0px'
+}
+
+const rightInput = {
+    flex:'5',
+    padding:'5px',
+    margin:'10px 0px 0px 10px'
+}
+
+export default AddUser;
