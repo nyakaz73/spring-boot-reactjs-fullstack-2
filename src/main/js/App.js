746762594a9942dadd0f351ac1881726aa81a09f
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-10 18:30:13 +0200

    Added axios actions DELETE, POST, ADD

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,58 +1,55 @@
-import React, { Component } from "react";
+import React, {Component, useCallback} from "react";
 import ReactDOM from "react-dom";
 import Header from '../../../frontend/src/components/layouts/Header';
 import RegisterUser from '../../../frontend/src/components/RegisterUser';
 import Users from '../../../frontend/src/components/Users';
-
+import axios from "axios";
 export class App extends Component {
-    state = {
-        users:[
-            {
-                id:1,
-                name:"John",
-                surname:"Doe",
-                email:"john@doe.com",
-                username:"johnny",
-                password:"strong-password"
-            },
-            {
-                id:2,
-                name:"Marcus",
-                surname:"Daniels",
-                email:"marc@dan.com",
-                username:"marcus",
-                password:"strong-password2"
-            },
-            {
-                id:3,
-                name:"Daniel",
-                surname:"Johns",
-                email:"dane@jonhs.com",
-                username:"danny",
-                password:"strong-password3"
-            }
-        ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            users:[
+            ]
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/user/all')
+            .then(response => this.setState({users:response.data}))
     }
 
     //Deleting User
     removeUser = (id) =>{
-        this.setState(
-            {users: [...this.state.users.filter(
-                    user => user.id !== id
-                )]}
-        );
+        axios.delete(`/user/${id}`)
+            .then(
+                response =>this.setState( //Updating UI
+                    {users: [...this.state.users.filter(
+                            user => user.id !== id
+                        )]}
+                )
+            );
     }
+
+    addUser = (newUser) =>{
+        axios.post('/user/save',newUser)
+            .then(
+                (response) =>{
+                    console.log(response.data);
+                    this.setState({users:[...this.state.users,response.data]})
+                }
+            );
+    }
+
     render() {
         return (
             <div className="container">
                 <Header/>
-                <RegisterUser/>
+                <RegisterUser addUser={this.addUser}/>
                 <Users users={this.state.users} removeUser={this.removeUser}/>
             </div>
         );
     }
 }
-
 
 
 export default App;
