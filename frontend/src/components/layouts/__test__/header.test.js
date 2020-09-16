bd6a50f3b59139421863d2f11bcbd1c86fe1a204
Tafadzwa Nyamukapa <tafadzwalnyamukapa@gmail.com>
2020-09-16 17:24:35 +0200

    Created Header.js test

---
+++ frontend/src/components/layouts/__test__/header.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Header from '../Header';
+import {act} from "@testing-library/react";
+
+let container;
+
+//Context Management
+beforeEach(()=>{
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(()=>{
+    document.body.removeChild(container);
+    container = null //resetting the DOM to empty
+});
+
+it("renders Header without crashing", ()=>{
+    //Test if Component is rendering
+    act(()=>{
+       ReactDOM.render(<Header/>,container);
+    });
+});
